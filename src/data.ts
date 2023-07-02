7a25c8f99f9efdabf1d0a6bce3fa0de50bdf5eaa
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2023-07-02 11:29:20 +0300

    refactor: delete template object from array

--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,3 @@
 import { Data } from './Types'
 
-export const Users: Data = [
-    {
-        id: '144ea26f-a8be-4d8c-a9f6-d8a8493f5dc0',
-        username: 'Sara',
-        age: 17,
-        hobbies: ['progging', 'gaming']
-    }
-]
+export const Users: Data = []
